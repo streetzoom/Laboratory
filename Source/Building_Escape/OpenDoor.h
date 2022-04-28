ad2b9fee35f398bcc82de66125f681d84f9d836d
Ilya Bril <brilillja81@gmail.com>
2022-04-28 21:49:35 +0300

    second part

---
+++ Source/Building_Escape/OpenDoor.h
@@ -0,0 +1,62 @@
+ // Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "UObject/UnrealType.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+	float TotalMassOfActors() const;
+	void FindAudioComponent();
+	void FindPressurePlate();
+
+	// Tracks whether the sound has been played.
+	bool OpenDoorSound  = false;
+	bool CloseDoorSound = true;
+	
+private:
+	float InitialYaw;
+	float CurrentYaw;
+
+	UPROPERTY(EditAnywhere)
+	float MassToOpenDoors = 50.f;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 1.f;
+
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.f;
+
+	float DoorLastOpened = 0.f;
+	
+	UPROPERTY(EditAnywhere)
+	float DoorOpenSpeed = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseSpeed = 2.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate = nullptr;
+
+	UPROPERTY()
+	UAudioComponent* AudioComponent = nullptr;
+};
